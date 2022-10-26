61ceb3be3a46143a2382a42d1e67aceedcd4788b
jonas <>
2022-10-26 15:13:09 +0200

    stuff from this mornings session

--- src/main/scala/amyc/parsing/Parser.scala
+++ src/main/scala/amyc/parsing/Parser.scala
@@ -24,19 +24,26 @@ object Parser extends Pipeline[Iterator[Token], Program] with Parsers {
   def op(string: String): Syntax[Token] = elem(OperatorKind(string))
   def kw(string: String): Syntax[Token] = elem(KeywordKind(string))
 
-  implicit def delimiter(string: String): Syntax[Token] = elem(DelimiterKind(string))
+  implicit def delimiter(string: String): Syntax[Token] = elem(
+    DelimiterKind(string)
+  )
 
   // An entire program (the starting rule for any Amy file).
-  lazy val program: Syntax[Program] = many1(many1(module) ~<~ eof).map(ms => Program(ms.flatten.toList).setPos(ms.head.head))
+  lazy val program: Syntax[Program] = many1(many1(module) ~<~ eof).map(ms =>
+    Program(ms.flatten.toList).setPos(ms.head.head)
+  )
 
   // A module (i.e., a collection of definitions and an initializer expression)
-  lazy val module: Syntax[ModuleDef] = (kw("object") ~ identifier ~ many(definition) ~ opt(expr) ~ kw("end") ~ identifier).map {
-    case obj ~ id ~ defs ~ body ~ _ ~ id1 => 
-      if id == id1 then 
-        ModuleDef(id, defs.toList, body).setPos(obj)
-      else 
-        throw new AmycFatalError("Begin and end module names do not match: " + id + " and " + id1)
-  }
+  lazy val module: Syntax[ModuleDef] =
+    (kw("object") ~ identifier ~ many(definition) ~ opt(expr) ~ kw(
+      "end"
+    ) ~ identifier).map { case obj ~ id ~ defs ~ body ~ _ ~ id1 =>
+      if id == id1 then ModuleDef(id, defs.toList, body).setPos(obj)
+      else
+        throw new AmycFatalError(
+          "Begin and end module names do not match: " + id + " and " + id1
+        )
+    }
 
   // An identifier.
   val identifier: Syntax[String] = accept(IdentifierKind) {
@@ -45,100 +52,130 @@ object Parser extends Pipeline[Iterator[Token], Program] with Parsers {
 
   // An identifier along with its position.
   val identifierPos: Syntax[(String, Position)] = accept(IdentifierKind) {
-    case id@IdentifierToken(name) => (name, id.position)
+    case id @ IdentifierToken(name) =>
+      (name, id.position) // @ is the Scala pattern matching operator
   }
 
   // A definition within a module.
-  lazy val definition: Syntax[ClassOrFunDef] = 
-    lazy val functionDefinition: Syntax[ClassOrFunDef] = 
-    (kw("fn") ~ identifier ~ parameters ~ typeTree ~ expr).map {
-      case kw ~ id ~ params ~ retType ~ body => FunDef(id,params,retType,body).setPos(kw)
-    }
-    lazy val abstractClassDefinition: Syntax[ClassOrFunDef] = 
-      (kw("abstract") ~ kw("class") ~ identifier).map {
-        case kw ~ _ ~ id => AbstractClassDef(id).setPos(kw)
+  lazy val definition: Syntax[ClassOrFunDef] =
+    lazy val functionDefinition: Syntax[ClassOrFunDef] =
+      (kw("fn") ~ identifier ~ parameters ~ typeTree ~ expr).map {
+        case kw ~ id ~ params ~ retType ~ body =>
+          FunDef(id, params, retType, body).setPos(kw)
+      }
+    lazy val abstractClassDefinition: Syntax[ClassOrFunDef] =
+      (kw("abstract") ~ kw("class") ~ identifier).map { case kw ~ _ ~ id =>
+        AbstractClassDef(id).setPos(kw)
       }
     lazy val caseClassDefinition: Syntax[ClassOrFunDef] =
-      (kw("case") ~ kw("class") ~ identifier ~ many(typeTree) ~ kw("extends") ~ identifier).map {
-        case kw ~ _ ~ id ~ params ~ _ ~ parent => CaseClassDef(id, params.toList, parent).setPos(kw)
+      (kw("case") ~ kw("class") ~ identifier ~ many(typeTree) ~ kw(
+        "extends"
+      ) ~ identifier).map { case kw ~ _ ~ id ~ params ~ _ ~ parent =>
+        CaseClassDef(id, params.toList, parent).setPos(kw)
       }
     functionDefinition | abstractClassDefinition | caseClassDefinition
-    
-  
-    
-  
+
   // A list of parameter definitions.
-  lazy val parameters: Syntax[List[ParamDef]] = repsep(parameter, ",").map(_.toList)
+  lazy val parameters: Syntax[List[ParamDef]] =
+    repsep(parameter, ",").map(_.toList)
 
   // A parameter definition, i.e., an identifier along with the expected type.
-  lazy val parameter: Syntax[ParamDef] = 
-    (identifier ~ kw(":") ~ typeTree).map {
-      case id ~ kw ~ tpe => ParamDef(id,tpe).setPos(kw)
+  lazy val parameter: Syntax[ParamDef] =
+    (identifier ~ kw(":") ~ typeTree).map { case id ~ kw ~ tpe =>
+      ParamDef(id, tpe).setPos(kw)
     }
-   
+
   // A type expression.
   lazy val typeTree: Syntax[TypeTree] = primitiveType | identifierType
 
   // A built-in type (such as `Int`).
-  val primitiveType: Syntax[TypeTree] = (accept(PrimTypeKind) {
-    case tk@PrimTypeToken(name) => TypeTree(name match {
-      case "Unit" => UnitType
-      case "Boolean" => BooleanType
-      case "Int" => IntType
-      case "String" => StringType
-      case _ => throw new java.lang.Error("Unexpected primitive type name: " + name)
-    }).setPos(tk)
-  } ~ opt("(" ~ literal ~ ")")).map { 
-    case (prim@TypeTree(IntType)) ~ Some(_ ~ IntLiteral(32) ~ _) => prim
-    case TypeTree(IntType) ~ Some(_ ~ IntLiteral(width) ~ _) => 
-      throw new AmycFatalError("Int type can only be used with a width of 32 bits, found : " + width)
-    case TypeTree(IntType) ~ Some(_ ~ lit ~ _) =>
-      throw new AmycFatalError("Int type should have an integer width (only 32 bits is supported)")
-    case TypeTree(IntType) ~ None => 
-      throw new AmycFatalError("Int type should have a specific width (only 32 bits is supported)")
-    case prim ~ Some(_) => 
-      throw new AmycFatalError("Only Int type can have a specific width")
-    case prim ~ None => prim
-  }
+  val primitiveType: Syntax[TypeTree] =
+    (accept(PrimTypeKind) { case tk @ PrimTypeToken(name) =>
+      TypeTree(name match {
+        case "Unit"    => UnitType
+        case "Boolean" => BooleanType
+        case "Int"     => IntType
+        case "String"  => StringType
+        case _ =>
+          throw new java.lang.Error("Unexpected primitive type name: " + name)
+      }).setPos(tk)
+    } ~ opt("(" ~ literal ~ ")")).map {
+      case (prim @ TypeTree(IntType)) ~ Some(_ ~ IntLiteral(32) ~ _) => prim
+      case TypeTree(IntType) ~ Some(_ ~ IntLiteral(width) ~ _) =>
+        throw new AmycFatalError(
+          "Int type can only be used with a width of 32 bits, found : " + width
+        )
+      case TypeTree(IntType) ~ Some(_ ~ lit ~ _) =>
+        throw new AmycFatalError(
+          "Int type should have an integer width (only 32 bits is supported)"
+        )
+      case TypeTree(IntType) ~ None =>
+        throw new AmycFatalError(
+          "Int type should have a specific width (only 32 bits is supported)"
+        )
+      case prim ~ Some(_) =>
+        throw new AmycFatalError("Only Int type can have a specific width")
+      case prim ~ None => prim
+    }
 
   // A user-defined type (such as `List`).
-  lazy val identifierType: Syntax[TypeTree] = 
-        ???
-    
+  // should reject reserved words?:
+  // true, false, case, class, abstract, object, types(int, string, ...). _ (wildcard)?
+  lazy val identifierType: Syntax[TypeTree] =
+    (identifier ~ opt(kw(".") ~ identifier)).map {
+      case ident1 ~ Some(ident2) => // ident2 is a tuple (., ident)
+        TypeTree(ClassType(QualifiedName(Some(ident1), ident2._2)))
+      case identifier ~ None =>
+        TypeTree(ClassType(QualifiedName(None, identifier)))
+    }
+
   // An expression.
   // HINT: You can use `operators` to take care of associativity and precedence
-  lazy val expr: Syntax[Expr] = recursive { 
-        ???
-     
+  lazy val expr: Syntax[Expr] = recursive {
+    ???
+
   }
-    
+
   // A literal expression.
-  lazy val literal: Syntax[Literal[_]] = 
-        ???
-    
+  lazy val literal: Syntax[Literal[_]] =
+    unitLiteral | otherLiteral
+
+  // Unit litteral
+  // probably messes the LL1 property
+  lazy val unitLiteral: Syntax[Literal[_]] =
+    kw("()").map { case kw => UnitLiteral() }
+
+  // Litterals that are not Units (LiteralKind)
+  lazy val otherLiteral: Syntax[Literal[_]] =
+    accept(LiteralKind) {
+      case IntLitToken(value)    => IntLiteral(value)
+      case StringLitToken(value) => StringLiteral(value)
+      case BoolLitToken(value)   => BooleanLiteral(value)
+    }
+
   // A pattern as part of a mach case.
-  lazy val pattern: Syntax[Pattern] = recursive { 
-        ???
-      }
-    
-  lazy val literalPattern: Syntax[Pattern] = 
-        ???
-    
-  lazy val wildPattern: Syntax[Pattern] = 
-        ???
-    
-    
+  lazy val pattern: Syntax[Pattern] = recursive {
+    ???
+  }
+
+//   lazy val patterns: Syntax[]
+
+  lazy val literalPattern: Syntax[Pattern] =
+    ???
+
+  lazy val wildPattern: Syntax[Pattern] =
+    (kw("_")).map { case kw =>
+      WildcardPattern().setPos(kw)
+    }
 
   // HINT: It is useful to have a restricted set of expressions that don't include any more operators on the outer level.
-  lazy val simpleExpr: Syntax[Expr] = 
-        literal.up[Expr] | variableOrCall | ???
-    
-    lazy val variableOrCall: Syntax[Expr] = ???
-  
+  lazy val simpleExpr: Syntax[Expr] =
+    literal.up[Expr] | variableOrCall | ???
+
+  lazy val variableOrCall: Syntax[Expr] = ???
 
   // TODO: Other definitions.
   //       Feel free to decompose the rules in whatever way convenient.
-
 
   // Ensures the grammar is in LL(1)
   lazy val checkLL1: Boolean = {
@@ -147,7 +184,7 @@ object Parser extends Pipeline[Iterator[Token], Program] with Parsers {
     } else {
       // Set `showTrails` to true to make Scallion generate some counterexamples for you.
       // Depending on your grammar, this may be very slow.
-      val showTrails = false
+      val showTrails = true
       debug(program, showTrails)
       false
     }
@@ -163,8 +200,13 @@ object Parser extends Pipeline[Iterator[Token], Program] with Parsers {
 
     parser(tokens) match {
       case Parsed(result, rest) => result
-      case UnexpectedEnd(rest) => fatal("Unexpected end of input.")
-      case UnexpectedToken(token, rest) => fatal("Unexpected token: " + token + ", possible kinds: " + rest.first.map(_.toString).mkString(", "))
+      case UnexpectedEnd(rest)  => fatal("Unexpected end of input.")
+      case UnexpectedToken(token, rest) =>
+        fatal(
+          "Unexpected token: " + token + ", possible kinds: " + rest.first
+            .map(_.toString)
+            .mkString(", ")
+        )
     }
   }
 }
