49be113308a4cc7f4ab095a6cbe94c5fa640135d
jonas <jonas.bonnaudet@gmail.com>
2022-12-12 20:54:55 +0100

    example now adds 2 numbers and prints it

--- src/main/scala/amyc/c/example.c
+++ src/main/scala/amyc/c/example.c
@@ -1,5 +1,8 @@
+#include <stdio.h>
+
 #define push stack[stack_pointer++] = 
 #define pop stack[--stack_pointer]
+#define cnst push 
 
 // maybe later do dynamic allocation
 #define MAX_STACK_SIZE 10000
@@ -8,21 +11,24 @@
 // #define binop(op) (push (pop op pop)) 
 // #define add_i32 binop(+) 
 
-#define cnst push 
-int main(){
-    int[MAX_STACK_SIZE] stack;
-    int stack_pointer = 0;
-    int[MAX_STACK_SIZE] locals;
-    int[MAX_STACK_SIZE] globals;
 
-    /*code*/
-    cnst 5; // const
-    push (pop + pop); // add_i32
-    sys_println1(pop);
-}
+// functions should be above because functions need to be declare before use
 
 int sys_println1(int x){
-    printf("%d", x);
+    printf("%d\n", x);
     return 0;
+}
+
+int main(){
+    int stack[MAX_STACK_SIZE];
+    int stack_pointer = 0;
+    int locals[MAX_STACK_SIZE];
+    int globals[MAX_STACK_SIZE];
+
+    /*code*/
+    cnst 5; // const 5
+    cnst 10; // const 10
+    push (pop + pop); // add_i32 5, 10
+    sys_println1(pop); // should print 15
 }
 
